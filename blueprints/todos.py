1d68d4ff820d9d2ef1a18e81aef982cb57b34645
most_welcome <tuadutta13@gmail.com>
2021-11-23 09:57:05 +0530

    added delete operation for todo items

--- blueprints/todos.py
+++ blueprints/todos.py
@@ -1,14 +1,28 @@
 from database.models.tables.todo import Todo
-from flask import Blueprint, render_template
-from managers.todos import add_todo, get_todos
+from flask import Blueprint, render_template, request, redirect, url_for
+from managers.todos import add_todo, get_todos, delete_todo
 
 TODOS_BLUEPRINT = Blueprint('todos', __name__)
 
 
-@TODOS_BLUEPRINT.route('/add')
+@TODOS_BLUEPRINT.route('/add', methods=['GET', 'POST'])
 def add_todo_item():
-    add_todo()
-    return render_template('index.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['description']
+        add_todo(title, desc)
+        return redirect(url_for('todos.get_todo_list'))
+
+
+@TODOS_BLUEPRINT.route('/update/<int:sno>')
+def update_todo_item(sno):
+    pass
+
+
+@TODOS_BLUEPRINT.route('/delete/<int:sno>')
+def delete_todo_item(sno):
+    delete_todo(sno)
+    return redirect(url_for('todos.get_todo_list'))
 
 
 @TODOS_BLUEPRINT.route('/')
--- managers/todos.py
+++ managers/todos.py
@@ -2,8 +2,9 @@ from database.db import db
 from database.models.tables.todo import Todo
 from datetime import datetime
 
-def add_todo():
-    todo = Todo(title='Start learning react', content='This is very important')
+
+def add_todo(title, description):
+    todo = Todo(title=title, content=description)
     db.session.add(todo)
     db.session.commit()
 
@@ -11,3 +12,13 @@ def add_todo():
 def get_todos():
     all_todo = Todo.query.all()
     return all_todo
+
+
+def delete_todo(sno):
+    todo_item = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo_item)
+    db.session.commit()
+
+
+def update_todo(sno):
+    pass
